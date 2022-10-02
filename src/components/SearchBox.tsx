937b2165ae67484f2a0ab0b35e85d3788a31bf54
woojeong kong <coolstevevc@gmail.com>
2022-10-02 22:46:29 +0900

    feat: Add SearchArea component styles

---
+++ src/components/SearchBox.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+
+function SearchBox() {
+  return (
+    <SearchWrapper>
+      <div className="icon-container">
+        <FontAwesomeIcon className="icon" icon={faMagnifyingGlass} />
+      </div>
+      <div className="text-container">
+        <input
+          type="text"
+          placeholder="배우고 싶은 언어, 기술을 검색해 보세요"
+        />
+      </div>
+    </SearchWrapper>
+  );
+}
+
+const SearchWrapper = styled.div`
+  display: flex;
+  width: 100%;
+  padding: 12px 0;
+  border: 1px solid rgb(201, 202, 204);
+  border-radius: 4px;
+
+  .icon-container {
+    .icon {
+      margin: 0px 12px;
+    }
+  }
+
+  .text-container {
+    width: 100%;
+
+    input {
+      width: 100%;
+      border: none;
+      outline: none;
+      box-sizing: border-box;
+
+      ::placeholder {
+        margin: 12px 0;
+        color: gray;
+      }
+    }
+  }
+`;
+
+export default SearchBox;
