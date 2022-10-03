edf995694f028321a70ad9dd26adf3304bdd8ed2
woojeong kong <coolstevevc@gmail.com>
2022-10-03 20:14:06 +0900

    feat: Add background color

--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -3,6 +3,10 @@ import { normalize } from "styled-normalize";
 
 const GlobalStyle = createGlobalStyle`
   ${normalize}
+
+  #root {
+    background-color: rgb(240, 241, 243);
+  }
 `;
 
 export default GlobalStyle;
